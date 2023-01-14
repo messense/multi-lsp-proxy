049a80519a9b1d63f4c828744b499c6f007b3fc3
messense <messense@icloud.com>
2023-01-14 18:11:53 +0800

    Add cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+use std::fs;
+use std::path::PathBuf;
+
+use anyhow::Result;
+use clap::Parser;
+
+use self::config::LspConfig;
+
 mod config;
 
+#[derive(Debug, Clone, Parser)]
+struct Cli {
+    /// configuration file path
+    #[arg(short = 'c', long, default_value = "config.toml")]
+    config: PathBuf,
+}
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let config_content = fs::read_to_string(&cli.config)?;
+    let lsp_config: LspConfig = toml_edit::easy::from_str(&config_content)?;
+    println!("{:#?}", lsp_config);
+    Ok(())
 }
